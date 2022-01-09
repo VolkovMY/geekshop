d55b14fdfdfd3edd40805613da153dff0d1d653d
Volkov Mkhail <volk.mixail@gmail.com>
2022-01-09 13:22:20 +0400

    lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+nowdate = datetime.date
+print(nowdate)
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    menu_links = [
+        {'view_name': 'index', 'name': 'домой'},
+        {'view_name': 'products', 'name': 'продукты'},
+        {'view_name': 'contact', 'name': 'контакт'},
+    ]
+    return render(request, 'mainapp/index.html', context={
+        'menu_links': menu_links})
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    menu_links = [
+        {'view_name': 'index', 'name': 'домой'},
+        {'view_name': 'products', 'name': 'продукты'},
+        {'view_name': 'contact', 'name': 'контакт'},
+    ]
+    return render(request, 'mainapp/contact.html', context={
+        'menu_links': menu_links})
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    menu_links = [
+        {'view_name': 'index', 'name': 'домой'},
+        {'view_name': 'products', 'name': 'продукты'},
+        {'view_name': 'contact', 'name': 'контакт'},
+    ]
+    links_menu = [
+        {'href': 'products', 'name': 'все'},
+        {'href': 'products', 'name': 'дом'},
+        {'href': 'products', 'name': 'офис'},
+        {'href': 'products', 'name': 'модерн'},
+        {'href': 'products', 'name': 'классика'},
+    ]
+    return render(request, 'mainapp/products.html', context={
+        'menu_links': menu_links, 'links_menu': links_menu})
